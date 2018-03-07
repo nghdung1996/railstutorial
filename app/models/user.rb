048c07a8699f1cc4ff5c237966ff87524193b0d0
nghdung1996 <huydung17101996@gmail.com>
2018-03-07 18:10:39 +0700

    Chap9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,46 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.password.minimum}
+  validates :password, presence: true, allow_nil: true,
+    length: {minimum: Settings.password.minimum}
 
   before_save :email_downcase
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 
   private
 
@@ -23,6 +55,6 @@ class User < ApplicationRecord
       else
         BCrypt::Engine.cost
       end
-      BCrypt::Password.create string, cost: cost
+    BCrypt::Password.create string, cost: cost
   end
 end
