2841d7982daf63e01c1db0324cf4757c3ed90442
nghdung1996 <huydung17101996@gmail.com>
2018-03-15 21:19:40 +0700

    Chap10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   attr_reader :user
 
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :admin_user, only: :destroy
+  before_action :find_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def new
     @user = User.new
   end
@@ -16,12 +25,23 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find_by id: params[:id]
+  def show; end
 
-    return if user
-    flash[:success] = t "error_user"
-    redirect_to root_path
+  def edit; end
+
+  def update
+    if user.update_attributes user_params
+      flash[:success] = t "updated"
+      redirect_to user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user.destroy
+    flash[:success] = t "deleted"
+    redirect_to users_url
   end
 
   private
@@ -29,5 +49,28 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".login_warning"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless user.current_user? current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+
+    return if user
+    flash[:success] = t "error_user"
+    redirect_to root_path
   end
 end
